5eed8f76fefd2dd347945500f32cd2bf11ade51a
oxqdu <oxqdu1337@gmail.com>
2025-12-09 17:17:31 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -2,36 +2,36 @@
 
 namespace global
 {
-	static void Initialize( GarrysMod::Lua::ILuaBase *LUA )
+	static void Initialize(GarrysMod::Lua::ILuaBase* LUA)
 	{
-		LUA->CreateTable( );
+		LUA->CreateTable();
 
-		LUA->PushString( "{MODULE NAME HERE} 1.0.0" );
-		LUA->SetField( -2, "Version" );
+		LUA->PushString("template 1.0.0");
+		LUA->SetField(-2, "Version");
 
 		// version num follows LuaJIT style, xxyyzz
-		LUA->PushNumber( 10000 );
-		LUA->SetField( -2, "VersionNum" );
+		LUA->PushNumber(10000);
+		LUA->SetField(-2, "VersionNum");
 
-		LUA->Push( -1 );
-		LUA->SetField( GarrysMod::Lua::INDEX_GLOBAL, "{MODULE NAME HERE}" );
-	}
+		LUA->Push(-1);
+		LUA->SetField(GarrysMod::Lua::INDEX_GLOBAL, "template");
+	};
 
-	static void Deinitialize( GarrysMod::Lua::ILuaBase *LUA )
+	static void Deinitialize(GarrysMod::Lua::ILuaBase* LUA)
 	{
-		LUA->PushNil( );
-		LUA->SetField( GarrysMod::Lua::INDEX_GLOBAL, "{MODULE NAME HERE}" );
-	}
-}
+		LUA->PushNil();
+		LUA->SetField(GarrysMod::Lua::INDEX_GLOBAL, "template");
+	};
+};
 
-GMOD_MODULE_OPEN( )
+GMOD_MODULE_OPEN()
 {
-	global::Initialize( LUA );
+	global::Initialize(LUA);
 	return 1;
-}
+};
 
-GMOD_MODULE_CLOSE( )
+GMOD_MODULE_CLOSE()
 {
-	global::Deinitialize( LUA );
+	global::Deinitialize(LUA);
 	return 0;
-}
+};
